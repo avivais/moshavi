fb833c3b6a11c1b6a90d3040b00bc7b4df80302c
Avi Vaisenberger <avi@aniview.com>
2025-03-12 00:43:08 +0200

    Esacpe regex question mark

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
         return [
             { source: "/media/(.*)", headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }] },
             { source: "/_next/image(.*)", headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }] },
-            { source: "/_next/image?url=%2Fmedia%2F(.*)", headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }] }, // Add this
+            { source: "/_next/image\?url=%2Fmedia%2F(.*)", headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }] }, // Add this
         ];
     },
 };
