6c0b8902429af50ec975bad0aa39558de77604f2
Avi Vaisenberger <avivais@gmail.com>
2025-03-11 18:01:23 +0000

    Allow caching media files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/media/(.*)", // Match all files under /media
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable", // 1 year, immutable
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
