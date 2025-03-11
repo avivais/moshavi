e129fac594b91f3e88141321d673d12385ad53a2
Avi Vaisenberger <avi@aniview.com>
2025-03-12 00:41:52 +0200

    Updated config to catch %2f and / for caching

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/media/(.*)", // Static files
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        source: "/_next/image(.*)", // Optimized images
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
+    async headers() {
+        return [
+            { source: "/media/(.*)", headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }] },
+            { source: "/_next/image(.*)", headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }] },
+            { source: "/_next/image?url=%2Fmedia%2F(.*)", headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }] }, // Add this
+        ];
+    },
 };
 
 export default nextConfig;
