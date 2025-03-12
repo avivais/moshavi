2e69e099faee1af9e578e0d1f7cc4ba17ec2b6f4
Avi Vaisenberger <avi@aniview.com>
2025-03-12 07:27:56 +0200

    Added the poster images files

--- app/sets/videoSets.ts
+++ app/sets/videoSets.ts
@@ -12,20 +12,20 @@ export const videoSets: VideoSet[] = [
         title: 'MoshAvi #001',
         date: '11.08.2023',
         src: '/media/sets/MoshAvi-001.mp4',
-        poster: '/media/sets/MoshAvi-001.jpg'
+        poster: '/media/poster/MoshAvi-001.jpg'
     },
     {
         id: 2,
         title: 'MoshAvi #002',
         date: '26.04.2024',
         src: '/media/sets/MoshAvi-002.mp4',
-        poster: '/media/sets/MoshAvi-002.jpg'
+        poster: '/media/poster/MoshAvi-002.jpg'
     },
     {
         id: 3,
         title: 'MoshAvi #003',
         date: '18.10.2024',
         src: '/media/sets/MoshAvi-003.mp4',
-        poster: '/media/sets/MoshAvi-003.jpg'
+        poster: '/media/poster/MoshAvi-003.jpg'
     }
 ]
