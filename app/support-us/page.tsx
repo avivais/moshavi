9fbf475258644f28e7c563df4276bb74cd3e7eb5
Avi Vaisenberger <avi@aniview.com>
2025-03-12 06:08:59 +0200

    Updated donations og text

--- app/support-us/page.tsx
+++ app/support-us/page.tsx
@@ -3,10 +3,10 @@ import SupportUsClient from './SupportUsClient' // We'll create this next
 
 export const metadata: Metadata = {
   title: 'MoshAvi Productions | Support Us',
-  description: 'תרקדו כאילו אין מחר ותתרמו כאילו יש עלויות',
+  description: 'תרקדו כאילו אין מחר | תתרמו כאילו יש עלויות',
   openGraph: {
     title: 'MoshAvi Productions | Support Us',
-    description: 'תרקדו כאילו אין מחר ותתרמו כאילו יש עלויות',
+    description: 'תרקדו כאילו אין מחר | תתרמו כאילו יש עלויות',
     url: 'https://www.moshavi.com/support-us',
     type: 'website',
     siteName: 'MoshAvi',
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
   twitter: {
     card: 'summary_large_image',
     title: 'MoshAvi Productions | Support Us',
-    description: 'תרקדו כאילו אין מחר ותתרמו כאילו יש עלויות',
+    description: 'תרקדו כאילו אין מחר | תתרמו כאילו יש עלויות',
     images: ['https://www.moshavi.com/media/og/support-us.jpg'],
   },
 }
