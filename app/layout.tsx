a4bab35ed01a3ba9bf7adbed1585e7f5382fdfbf
Avi Vaisenberger <avi@aniview.com>
2025-03-25 11:24:19 +0200

    Added Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { poiretOne, karantina, bonheurRoyale, hennyPenny } from './fonts'
 import './globals.css'
 import Nav from '@/components/Nav'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function RootLayout({
     children,
@@ -12,6 +13,9 @@ export default function RootLayout({
             <body className={`bg-black text-white ${poiretOne.variable} ${karantina.variable} ${bonheurRoyale.variable} ${hennyPenny.variable}`}>
                 <Nav />
                 <div className="pt-12">{children}</div>
+                {process.env.NEXT_PUBLIC_GA_ID && (
+                    <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+                )}
             </body>
         </html>
     )
