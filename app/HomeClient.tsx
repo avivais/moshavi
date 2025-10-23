35d0a4c78fd6b463cb7c1f8e880489ca79ab6398
Avi Vaisenberger <avi@aniview.com>
2025-10-23 21:52:11 +0300

    Updated to 007 TBD

--- app/HomeClient.tsx
+++ app/HomeClient.tsx
@@ -56,8 +56,8 @@ export default function HomeClient() {
     return (
         <main className="min-h-screen flex flex-col items-center p-0">
             <section className="mb-4 text-center px-4 font-poiret-one">
-                <div className="text-3xl">MoshAvi #006</div>
-                <div className="text-xl">17.10.2025</div>
+                <div className="text-3xl">MoshAvi #007</div>
+                <div className="text-xl">TBD</div>
                 <div className="text-lg font-bonheur-royale">Music Is The Answer</div>
             </section>
 
